c46ec78a310b0bf395c75cb44ec8bda50f9f87ca
Monojit Datta <datta.monojit@gmail.com>
2018-09-13 02:59:14 +0530

    Modified npm install and link with HttpConnection components for Production

--- luisbinder/internal/CMPLUISBinder.js
+++ luisbinder/internal/CMPLUISBinder.js
@@ -2,7 +2,7 @@
 
 const CMPLUISConstants = require("./CMPLUISConstants");
 const HttpConnectionRef = require("../node_modules/https_connection_binder");
-const Utils = require("../node_modules/utility_helper");
+const Utils = require("./utility_helper");
 
 class CMPLUISBinder
 {
